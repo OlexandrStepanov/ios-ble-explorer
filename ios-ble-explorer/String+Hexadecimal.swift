9822e296775334489504d02cce8573c14da41443
Oleksandr Stepanov <Oleksandr.Stepanov@akqa.com>
2017-09-26 11:11:15 +0300

    Converted to Swift3 syntax. Added filter peripherals logic.

--- ios-ble-explorer/String+Hexadecimal.swift
+++ ios-ble-explorer/String+Hexadecimal.swift
@@ -11,35 +11,17 @@ import Foundation
 extension String {
     
     /// Create NSData from hexadecimal string representation
-    ///
-    /// This takes a hexadecimal representation and creates a NSData object. Note, if the string has any spaces, those are removed. Also if the string started with a '<' or ended with a '>', those are removed, too. This does no validation of the string to ensure it's a valid hexadecimal string
-    ///
-    /// The use of `strtoul` inspired by Martin R at http://stackoverflow.com/a/26284562/1271826
-    ///
-    /// :returns: NSData represented by this hexadecimal string. Returns nil if string contains characters outside the 0-9 and a-f range.
-    
-    func dataFromHexadecimalString() -> NSData? {
-        let trimmedString = self.stringByTrimmingCharactersInSet(NSCharacterSet(charactersInString: "<> ")).stringByReplacingOccurrencesOfString(" ", withString: "")
-        
-        // make sure the cleaned up string consists solely of hex digits, and that we have even number of them
-        
-        var error: NSError?
-        let regex = NSRegularExpression(pattern: "^[0-9a-f]*$", options: .CaseInsensitive, error: &error)
-        let found = regex?.firstMatchInString(trimmedString, options: nil, range: NSMakeRange(0, count(trimmedString)))
-        if found == nil || found?.range.location == NSNotFound || count(trimmedString) % 2 != 0 {
-            return nil
+    func dataFromHexadecimalString() -> Data? {
+        var hex = self
+        var data = Data()
+        while(hex.characters.count > 0) {
+            let c: String = hex.substring(to: hex.index(hex.startIndex, offsetBy: 2))
+            hex = hex.substring(from: hex.index(hex.startIndex, offsetBy: 2))
+            var ch: UInt32 = 0
+            Scanner(string: c).scanHexInt32(&ch)
+            var char = UInt8(ch)
+            data.append(&char, count: 1)
         }
-        
-        // everything ok, so now let's build NSData
-        
-        let data = NSMutableData(capacity: count(trimmedString) / 2)
-        
-        for var index = trimmedString.startIndex; index < trimmedString.endIndex; index = index.successor().successor() {
-            let byteString = trimmedString.substringWithRange(Range<String.Index>(start: index, end: index.successor().successor()))
-            let num = UInt8(byteString.withCString { strtoul($0, nil, 16) })
-            data?.appendBytes([num] as [UInt8], length: 1)
-        }
-        
         return data
     }
     
@@ -51,9 +33,9 @@ extension String {
     ///
     /// :returns: String represented by this hexadecimal string. Returns nil if string contains characters outside the 0-9 and a-f range or if a string cannot be created using the provided encoding
     
-    func stringFromHexadecimalStringUsingEncoding(encoding: NSStringEncoding) -> String? {
+    func stringFromHexadecimalStringUsingEncoding(_ encoding: String.Encoding) -> String? {
         if let data = dataFromHexadecimalString() {
-            return NSString(data: data, encoding: encoding) as? String
+            return String.init(data: data, encoding: encoding)
         }
         
         return nil
@@ -65,8 +47,8 @@ extension String {
     ///
     /// :returns: String representation of this String object.
     
-    func hexadecimalStringUsingEncoding(encoding: NSStringEncoding) -> String? {
-        let data = dataUsingEncoding(NSUTF8StringEncoding)
+    func hexadecimalStringUsingEncoding(_ encoding: String.Encoding) -> String? {
+        let data = self.data(using: String.Encoding.utf8)
         return data?.hexadecimalString()
     }
 }
